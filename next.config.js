8f030576053b191ae576f27e94fd6bc560c33d09
Dulara Abeysuriya <abey0009@algonquinlive.com>
2024-08-05 16:07:54 -0400

    To resolve the Netlify build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
+// next.config.js
 module.exports = {
-  images: {
-    domains: [], // Add domains if you are using external images
-    minimumCacheTTL: 60, // Optional: Set cache TTL for images
-  },
+  // Other Next.js config options
+  trailingSlash: true, // Ensures URLs end with a trailing slash
 };
